3965dcf0e26215c6ddced52e1aafda4ed09632c2
Monyjenni <chansovanmonyyoeun03@gmail.com>
2023-08-06 19:39:38 +0700

    Fixing(Algo): Caesar cipher

--- rsamath/src/App.vue
+++ rsamath/src/App.vue
@@ -31,13 +31,44 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="4">
-          <v-btn @click="encryptMessage" color="primary">Encrypt</v-btn>
-        </v-col>
+        <!-- <v-row>
+          <v-col cols="4">
+            <v-subheader>Encrypted message</v-subheader>
+          </v-col>
+        </v-row> -->
+        <v-row>
+          <v-col cols="4">
+            <div class="text-fields-container">
+              <v-col cols="12">
+                <v-text-field
+                  label="Encrypted Output"
+                  filled
+                  rounded
+                  dense
+                  v-model="encryptedMessage"
+                ></v-text-field>
+              </v-col>
+              <v-spacer></v-spacer>
+            </div>
+          </v-col>
+          <v-col cols="8">
+            <div class="text-fields-container">
+              <v-col cols="6">
+                <v-text-field
+                  label="Decrypted Output"
+                  filled
+                  rounded
+                  densee
+                  v-model="decryptedMessage"
+                ></v-text-field>
+              </v-col>
+            </div>
+          </v-col>
+        </v-row>
       </v-row>
       <v-row>
         <v-col cols="4">
-          <v-subheader>Encrypted message</v-subheader>
+          <v-subheader>Converted to Numbers</v-subheader>
         </v-col>
       </v-row>
       <v-row>
@@ -45,11 +76,11 @@
           <div class="text-fields-container">
             <v-col cols="12">
               <v-text-field
-                label="Encrypted Output"
+                label="Numbers Output"
                 filled
                 rounded
                 dense
-                v-model="encryptedMessage"
+                v-model="numbersOutput"
               ></v-text-field>
             </v-col>
             <v-spacer></v-spacer>
@@ -58,28 +89,9 @@
       </v-row>
       <v-row>
         <v-col cols="4">
-          <v-btn @click="decryptMessage" color="primary">Decrypt</v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="4">
-          <v-subheader>Decrypted message</v-subheader>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="4">
-          <div class="text-fields-container">
-            <v-col cols="12">
-              <v-text-field
-                label="Decrypted Output"
-                filled
-                rounded
-                densee
-                v-model="decryptedMessage"
-              ></v-text-field>
-            </v-col>
-            <v-spacer></v-spacer>
-          </div>
+          <v-btn @click="encryptMessage" color="primary">Encrypt</v-btn>
+          <v-btn @click="decryptMessage" class="d-button" color="primary">Decrypt</v-btn>
+          <v-btn @click="clearMessage" class="clear-button" color="red">Clear</v-btn>
         </v-col>
       </v-row>
     </v-main>
@@ -93,76 +105,112 @@ export default {
     message: '', // User input message
     encryptedMessage: '', // Encrypted message result
     decryptedMessage: '', // Decrypted message result
+    numbersOutput: '', // Numbers representation of the original message
   }),
   methods: {
+    convertToNumbers(message) {
+      const k = 21; // New shift value for Caesar cipher
+      const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // All capital letters
+      const encryptedIndexes = [];
+
+      for (let i = 0; i < message.length; i++) {
+        const char = message[i];
+        const index = letters.indexOf(char.toUpperCase());
+
+        if (index !== -1) {
+          const encryptedIndex = (index + k) % 26;
+          encryptedIndexes.push(encryptedIndex);
+        }
+      }
+
+      return encryptedIndexes;
+    },
     encryptMessage() {
       const message = this.message; // Get the user input message
       const encryptedMessage = this.classicalEncrypt(message);
 
       // Update the encryptedMessage data property
       this.encryptedMessage = encryptedMessage;
+
+      // Convert the original message to numbers and update the numbersOutput data property
+      const encryptedIndexes = this.convertToNumbers(message);
+      this.numbersOutput = encryptedIndexes.join(', ');
     },
+    
     decryptMessage() {
       const encryptedMessage = this.encryptedMessage; // Get the encrypted message
       const decryptedMessage = this.classicalDecrypt(encryptedMessage);
 
       // Update the decryptedMessage data property
       this.decryptedMessage = decryptedMessage;
+
+      // Convert the decrypted message to numbers and update the numbersOutput data property
+      const numbers = this.convertToNumbers(decryptedMessage);
+      this.numbersOutput = numbers.join(', ');
     },
     classicalEncrypt(message) {
-      // Perform classical encryption algorithm here
-      // For demonstration, we'll use a simple Caesar cipher
-
-      const shift = 5; // Shift value for Caesar cipher
-      let encryptedMessage = '';
+      const k = 21; // New shift value for Caesar cipher
+      const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // All capital letters
+      const encryptedMessage = [];
 
       for (let i = 0; i < message.length; i++) {
-        const charCode = message.charCodeAt(i);
+        const char = message[i];
+        const index = letters.indexOf(char.toUpperCase());
 
-        // Encrypt each character by shifting it by the specified value
-        if (charCode >= 65 && charCode <= 90) {
-          // Uppercase letters
-          encryptedMessage += String.fromCharCode(((charCode - 65 + shift) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) {
-          // Lowercase letters
-          encryptedMessage += String.fromCharCode(((charCode - 97 + shift) % 26) + 97);
+        if (index !== -1) {
+          const encryptedIndex = (index + k) % 26;
+          console.log(encryptedIndex)
+          const encryptedChar = letters[encryptedIndex];
+          encryptedMessage.push(encryptedChar);
         } else {
-          // Non-alphabetic characters remain unchanged
-          encryptedMessage += message[i];
+          // For non-alphabetic characters, keep them as they are
+          encryptedMessage.push(char);
         }
       }
 
-      return encryptedMessage;
+      return encryptedMessage.join('');
     },
-    classicalDecrypt(encryptedMessage) {
-      // Perform decryption to reverse the encryption algorithm
-      // For demonstration, we'll reverse the Caesar cipher
 
-      const shift = 5; // Shift value for Caesar cipher
-      let decryptedMessage = '';
+    classicalDecrypt(encryptedMessage) {
+      const k = 21; // Shift value for decryption (inverse of encryption)
+      const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // All capital letters
+      const decryptedMessage = [];
 
       for (let i = 0; i < encryptedMessage.length; i++) {
-        const charCode = encryptedMessage.charCodeAt(i);
+        const char = encryptedMessage[i];
+        const index = letters.indexOf(char.toUpperCase());
 
-        // Decrypt each character by shifting it back by the specified value
-        if (charCode >= 65 && charCode <= 90) {
-          // Uppercase letters
-          decryptedMessage += String.fromCharCode(((charCode - 65 - shift + 26) % 26) + 65);
-        } else if (charCode >= 97 && charCode <= 122) {
-          // Lowercase letters
-          decryptedMessage += String.fromCharCode(((charCode - 97 - shift + 26) % 26) + 97);
+        if (index !== -1) {
+          const decryptedIndex = (index - k + 26) % 26; // Fix the decryption formula
+          console.log(decryptedIndex )
+          const decryptedChar = letters[decryptedIndex];
+          decryptedMessage.push(decryptedChar);
         } else {
-          // Non-alphabetic characters remain unchanged
-          decryptedMessage += encryptedMessage[i];
+          // For non-alphabetic characters, keep them as they are
+          decryptedMessage.push(char);
         }
       }
 
-      return decryptedMessage;
+      return decryptedMessage.join('');
+    },
+
+    clearMessage() {
+      // Clear the input and output fields
+      this.message = '';
+      this.decryptedMessage = '';
+      this.numbersOutput = '';
+      this.encryptedMessage = '';
     },
   },
 };
 </script>
-
 <style>
-/* Your styles here */
+/* Add some margin to the clear-button class */
+.clear-button {
+  margin-left: 16px; /* Adjust the value as needed for desired spacing */
+}
+.d-button {
+  margin-left: 16px
+}
+/* Your other styles here */
 </style>
